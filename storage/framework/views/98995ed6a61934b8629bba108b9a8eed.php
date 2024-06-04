f95a6bc3d79474de80f49c7ce92405ad2a43f7f5
dwyn25 <164441981+dwyn25@users.noreply.github.com>
2024-06-04 12:03:00 +0700

    Add files via upload

---
+++ storage/framework/views/98995ed6a61934b8629bba108b9a8eed.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>BERITA</title>
+    </head>
+    <body>
+        
+        <?php $__env->startSection('container'); ?>
+        <center><h1>Ini Halaman Berita</h1></center>
+        <h3>Judul Berita</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut dapibus magna. 
+        Vestibulum ornare molestie ante, nec imperdiet velit facilisis sed. Fusce rutrum, mi in 
+        malesuada viverra, sapien dolor dapibus neque, vehicula tincidunt velit nibh non neque. 
+        Maecenas eleifend, quam id pulvinar molestie, est risus finibus sem, non tristique ex 
+        purus nec nisi. Quisque mollis, velit et maximus maximus, magna eros ultrices nulla, 
+        eget imperdiet felis turpis sit amet arcu. Nullam fringilla risus sed ante maximus, at 
+        pellentesque arcu dapibus. Sed et sodales lacus. Aliquam lacinia consequat urna vel 
+        suscipit. Cras a est pharetra, elementum purus non, sodales sem.</p>
+        <?php $__env->stopSection(); ?>
+    </body>
+</html>
+<?php echo $__env->make('layout.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\dwyn\Latihan\resources\views/berita.blade.php ENDPATH**/ ?>
